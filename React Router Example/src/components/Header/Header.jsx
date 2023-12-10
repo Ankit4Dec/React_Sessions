1d66a89fee437fe29d1b5d3792397b03b8ca9545
Ankit4Dec <ankit27866@gmail.com>
2023-12-10 20:35:26 +0530

    css module to header part

--- React Router Example/src/components/Header/Header.jsx
+++ React Router Example/src/components/Header/Header.jsx
@@ -1,25 +1,37 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import routes from "./../../routes/routes.json";
+import styles from "./Header.module.css";
 
 const Header = () => {
+  const linkClasses = ({ isActive }) =>
+    isActive ? styles.activeLink : styles.linkStyle;
+
   return (
     <header style={{ marginBottom: "20px" }}>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">React App</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link>
-              <Link to={routes.HOME}>Home</Link>{" "}
+              <Link to={routes.HOME} className={linkClasses}>
+                Home
+              </Link>{" "}
             </Nav.Link>
             <Nav.Link>
-              <Link to={routes.ABOUT}>About</Link>{" "}
+              <Link to={routes.ABOUT} className={linkClasses}>
+                About
+              </Link>{" "}
             </Nav.Link>
             <Nav.Link>
-              <Link to={routes.PRODUCTS}>Products</Link>{" "}
+              <Link to={routes.PRODUCTS} className={linkClasses}>
+                Products
+              </Link>{" "}
             </Nav.Link>
             <Nav.Link>
-              <Link to={routes.CONTACT_US}>Contact Us</Link>{" "}
+              <Link to={routes.CONTACT_US} className={linkClasses}>
+                Contact Us
+              </Link>{" "}
             </Nav.Link>
           </Nav>
         </Container>
