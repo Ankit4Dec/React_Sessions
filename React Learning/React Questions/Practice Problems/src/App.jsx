a8b5da8e139e6df6463377f1082794ef485ba23e
Ankit <ankit27866@gmail.com>
2024-07-01 21:30:33 +0530

    Basic Hello

---
+++ React Learning/React Questions/Practice Problems/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { DayOne } from "./components/DayOne";
+
+function App() {
+  return (
+    <>
+      <DayOne />
+    </>
+  );
+}
+
+export default App;
---
+++ React Learning/React Questions/Practice Problems/src/components/DayOne.jsx
@@ -0,0 +1,7 @@
+export const DayOne = () => {
+  return (
+    <div>
+      <h2>Hello World !!! </h2>
+    </div>
+  );
+};
